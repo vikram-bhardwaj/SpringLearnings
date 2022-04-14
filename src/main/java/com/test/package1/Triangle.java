f8f98d0b94e0771cfa27136c1942a2661b0d9eea
vikram-bhardwaj <vikrambhardwaj94@gmail.com>
2022-04-15 04:26:09 +0530

    removing normal beans and adding collections

--- src/main/java/com/test/package1/Triangle.java
+++ src/main/java/com/test/package1/Triangle.java
@@ -1,46 +1,26 @@
 package com.test.package1;
 
+import java.util.List;
+
 public class Triangle
 {
-	private Point pointA;
-	private Point pointB;
-	private Point pointC;
+	private List<Point> points;
 	
-	public Point getPointA()
+	public List<Point> getPoints()
 	{
-		return pointA;
+		return points;
 	}
 
-	public void setPointA(Point pointA)
+	public void setPoints(List<Point> points)
 	{
-		this.pointA = pointA;
+		this.points = points;
 	}
 
-	public Point getPointB()
-	{
-		return pointB;
-	}
-
-	public void setPointB(Point pointB)
-	{
-		this.pointB = pointB;
-	}
-
-	public Point getPointC()
-	{
-		return pointC;
-	}
-
-	public void setPointC(Point pointC)
-	{
-		this.pointC = pointC;
-	}
-
-	
 	public void draw()
 	{
-		System.out.println("Point A object, value of X = " + getPointA().getX() + "| Point A object, value of Y = " + getPointA().getY());
-		System.out.println("Point B object, value of X = " + getPointB().getX() + "| Point B object, value of Y = " + getPointB().getY());
-		System.out.println("Point C object, value of X = " + getPointC().getX() + "| Point C object, value of Y = " + getPointC().getY());
+		for(Point point: points)
+		{
+			System.out.println("Point A object, value of X = " + point.getX() + "| Point A object, value of Y = " + point.getY());
+		}
 	}
 }
