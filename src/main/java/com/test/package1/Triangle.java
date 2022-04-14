d8e81291ef2ba1f68d2a674cbac2e79bbd506e17
vikram-bhardwaj <vikrambhardwaj94@gmail.com>
2022-04-15 04:55:55 +0530

    autowire by name

--- src/main/java/com/test/package1/Triangle.java
+++ src/main/java/com/test/package1/Triangle.java
@@ -1,26 +1,46 @@
 package com.test.package1;
 
-import java.util.List;
-
 public class Triangle
 {
-	private List<Point> points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 	
-	public List<Point> getPoints()
+	public Point getPointA()
 	{
-		return points;
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points)
+	public void setPointA(Point pointA)
 	{
-		this.points = points;
+		this.pointA = pointA;
 	}
 
+	public Point getPointB()
+	{
+		return pointB;
+	}
+
+	public void setPointB(Point pointB)
+	{
+		this.pointB = pointB;
+	}
+
+	public Point getPointC()
+	{
+		return pointC;
+	}
+
+	public void setPointC(Point pointC)
+	{
+		this.pointC = pointC;
+	}
+
+	
 	public void draw()
 	{
-		for(Point point: points)
-		{
-			System.out.println("Point A object, value of X = " + point.getX() + "| Point A object, value of Y = " + point.getY());
-		}
+		System.out.println("Point A object, value of X = " + getPointA().getX() + "| Point A object, value of Y = " + getPointA().getY());
+		System.out.println("Point B object, value of X = " + getPointB().getX() + "| Point B object, value of Y = " + getPointB().getY());
+		System.out.println("Point C object, value of X = " + getPointC().getX() + "| Point C object, value of Y = " + getPointC().getY());
 	}
 }
